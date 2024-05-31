c09bc07f7c326b2234db27cffb97d39e7540fc37
Tom Serrante <tserrante@gmail.com>
2024-05-31 00:35:15 -0400

    multiplication table

--- StartingOut/ch4_LoopsAndFiles/1_MultiplicationTable/Program.cs
+++ StartingOut/ch4_LoopsAndFiles/1_MultiplicationTable/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace MultiplicationTable;
+
+public class Program
+{
+    /*
+        todo
+        handle null cases better
+        data input validation
+    */
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num1 = int.Parse(System.Console.ReadLine());
+        Console.Write("Enter a second number: ");
+        int num2 = int.Parse(System.Console.ReadLine());
+
+        for(int i = 0; i < num2 + 1; i++)
+        {
+            System.Console.WriteLine($"{num1} x {i} = {num1*i}");
+        }
+
+        Console.Read();
+    }
+}
