8e7f879581841f65cdf0247d624b74e37d32e346
Tom Serrante <tserrante@gmail.com>
2024-05-31 19:26:18 -0400

    problems 3-4

---
+++ StartingOut/ch4_LoopsAndFiles/2_DistanceTraveled/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DistanceTraveled;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Distance Calculator");
+        bool inputValid = false;
+
+        while(!inputValid)
+        {
+            Console.Write("Enter the objects speed: ");
+            
+            bool speedCaptured = int.TryParse(Console.ReadLine(), out int speed);
+
+            Console.Write("Enter number of hours the object traveled: ");
+
+            bool hoursCaptured = int.TryParse(Console.ReadLine(), out int hours);
+
+            if(hours >= 1 && speed >= 0)
+            {
+                Console.WriteLine("Hour\tDistance Traveled");
+                Console.WriteLine("------------------------");
+                for(int i = 0; i < hours; i++)
+                {
+                    Console.WriteLine("{0}\t\t{1}", i+1, (i+1) * speed);
+                }
+                inputValid = true;
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Input was invalid, restarting.");
+            }
+
+        }
+    }
+}
---
+++ StartingOut/ch4_LoopsAndFiles/3_DistanceFile/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DistanceFile;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Distance Calculator");
+        bool inputValid = false;
+        string baseFilename = "distance";
+        while(!inputValid)
+        {
+            Console.Write("Enter the objects speed: ");
+            
+            bool speedCaptured = int.TryParse(Console.ReadLine(), out int speed);
+
+            Console.Write("Enter number of hours the object traveled: ");
+
+            bool hoursCaptured = int.TryParse(Console.ReadLine(), out int hours);
+
+            if(hours >= 1 && speed >= 0)
+            {
+                string date = DateTime.Now.ToShortDateString().Replace(@"/", "_");
+                string time = DateTime.Now.ToShortTimeString().Replace(":", string.Empty);
+                string fileType = ".txt";
+                string newFileName = string.Concat(baseFilename, "_", date, "_", time, fileType);
+                using(StreamWriter streamWriter = File.CreateText(newFileName))
+                {
+                    streamWriter.WriteLine("Hour\tDistance Traveled");
+                    streamWriter.WriteLine("------------------------");
+                    for(int i = 0; i < hours; i++)
+                    {
+                        streamWriter.WriteLine("{0}\t\t{1}", i+1, (i+1) * speed);
+                    }
+                    streamWriter.Close();
+                }
+                inputValid = true;
+                Console.WriteLine($"File {newFileName} created.  Open file to see output.");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Input was invalid, restarting.");
+            }
+
+        }
+    }
+}
